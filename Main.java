d96c2db584da7a6fd45753bc82fb9d40c9bbacc5
ahmed <ahmadabdullah4919@gmail.com>
2022-10-14 21:31:33 +0500

    Add Separate Menu Class

--- Main.java
+++ Main.java
@@ -1,16 +1,15 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 
 public class Main {
     
-    static String employeeFilePath = "E:\\Java\\LabManagement\\data\\employee.csv";
-    static String pcFilePath = "E:\\Java\\LabManagement\\data\\pc.csv";
-    static String labFilePath = "E:\\Java\\LabManagement\\data\\lab.csv";
+    static File employeeFile = new File("E:\\Java\\LabManagement\\data\\employee.csv");
+    static File pcFile = new File("E:\\Java\\LabManagement\\data\\pc.csv");
+    static File labFile = new File("E:\\Java\\LabManagement\\data\\lab.csv");
     
     public static void main(String[] args) throws IOException {
         
@@ -24,133 +23,20 @@ public class Main {
         loadData(department);
 
         // Print Menu
-        menu(department);
+        Menu.menu(department);
+
+        // Save Data
+        saveData(department);
     }
 
-    public static void menu(Department department) throws IOException{
-        
-        Scanner sc = new Scanner(System.in);
-        
-        while (true) {
-            System.out.println(String.format("%32s","").replace(" ", "="));
-            System.out.println(String.format("%17s", "MENU"));
-            System.out.println(String.format("%32s","").replace(" ", "="));
-            System.out.println("1. Add a Lab");
-            System.out.println("2. Remove a Lab");
-            System.out.println("3. Add PC to a Lab");
-            System.out.println("4. Remove PC from a Lab");
-            System.out.println("5. Print Labs");
-            System.out.println("6. Print PCs of a Lab");
-            System.out.println("7. Print Department Info");
-            System.out.println("8. Exit Program");
-            System.out.println(String.format("%32s","").replace(" ", "="));
-            System.out.println("Enter your choice: ");
-            int choice = sc.nextInt();
-            sc.nextLine();
-            System.out.println(String.format("%32s","").replace(" ", "="));
-
-            switch (choice) {
-                case 1:
-                    System.out.println("Enter Lab Name: ");
-                    String labName = sc.nextLine();
-                    System.out.println("Enter Lab Attendant First Name: ");
-                    String labAFirstName = sc.nextLine();
-                    System.out.println("Enter Lab Attendant Last Name: ");
-                    String labALastNAme = sc.nextLine();
-                    department.addLab(new Lab(labName, new Employee(labAFirstName, labALastNAme, "Lab Attendant")));
-                    System.out.println("Lab added successfully!\n");
-                    break;
-                case 2:
-                    System.out.println("Enter Lab Name: ");
-                    String name = sc.nextLine();
-                    int index = department.searchLab(name);
-                    if (index != -1) {
-                        department.removeLab(department.getLabs()[index]);
-                        System.out.println("Lab removed successfully!\n");
-                    } else {
-                        System.out.println("Lab not found!\n");
-                    }
-                    break;
-                case 3:
-                    System.out.println("Enter Lab Name: ");
-                    String labName1 = sc.nextLine();
-                    int index1 = department.searchLab(labName1);
-                    if (index1 != -1) {
-                        System.out.println("Enter PC Name: ");
-                        String pcName = sc.nextLine();
-                        System.out.println("Enter LCD Name: ");
-                        String monitorName = sc.nextLine();
-                        System.out.println("Enter RAM Size (GB): ");
-                        int ramSize = sc.nextInt();
-                        System.out.println("Enter SSD Size (GB): ");
-                        int hardDiskSize = sc.nextInt();
-                        System.out.println("Is PC has a graphic card? (true/false): ");
-                        boolean hasGraphicCard = sc.nextBoolean();
-                        department.getLabs()[index1].addPC(new PC(pcName, monitorName, ramSize, hardDiskSize, hasGraphicCard));
-                        System.out.println("PC added successfully!\n");
-                    } else {
-                        System.out.println("Lab not found!\n");
-                    }
-                    break;
-
-                case 4:
-                    System.out.println("Enter Lab Name: ");
-                    String labName2 = sc.nextLine();
-                    int index2 = department.searchLab(labName2);
-                    if (index2 != -1) {
-                        System.out.println("Enter PC Name: ");
-                        String pcName = sc.nextLine();
-                        int index3 = department.getLabs()[index2].searchPC(pcName);
-                        if (index3 != -1) {
-                            department.getLabs()[index2].removePC(department.getLabs()[index2].getPCs()[index3]);
-                            System.out.println("PC removed successfully!\n");
-                        } else {
-                            System.out.println("PC not found!\n");
-                        }
-                    } else {
-                        System.out.println("Lab not found!\n");
-                    }
-                    break;
-
-                case 5:
-                    department.printLabs();
-                    break;
-
-                case 6:
-                    System.out.println("Enter Lab Name: ");
-                    String labName3 = sc.nextLine();
-                    int index4 = department.searchLab(labName3);
-                    if (index4 != -1) {
-                        department.getLabs()[index4].printPCs();
-                    } else {
-                        System.out.println("Lab not found!\n");
-                    }
-                    break;
-
-                case 7:
-                    System.out.println(department);
-                    break;
-
-                case 8:
-                    sc.close();
-                    saveData(department);  // Save the Data
-                    System.out.println("Program Exited.");
-                    System.exit(0);
-                
-                default:
-                    System.out.println("Invalid Input!");
-                    break;
-            }
-        }
-    }
-
+    
+    // To Load Data in memory from files
     public static void loadData(Department department) throws IOException {
         
         System.out.println("Loading Data...");
 
         // Read data from employees.csv
-        FileReader fr = new FileReader(new File(employeeFilePath));
-        BufferedReader br = new BufferedReader(fr);
+        BufferedReader br = new BufferedReader(new FileReader(employeeFile));
         br.readLine();
         Employee[] employees = new Employee[50];  // Temporary Array
         String line;
@@ -161,8 +47,7 @@ public class Main {
         br.close();
 
         // Read data from labs.csv
-        fr = new FileReader(new File(labFilePath));
-        br = new BufferedReader(fr);
+        br = new BufferedReader(new FileReader(labFile));
         br.readLine();
         while ((line=br.readLine()) != null) {
             String[] fields = line.split(",");
@@ -171,8 +56,7 @@ public class Main {
         br.close();
 
         // Read data from pc.csv
-        fr = new FileReader(pcFilePath);
-        br = new BufferedReader(fr);
+        br = new BufferedReader(new FileReader(pcFile));
         br.readLine();
         while ((line=br.readLine()) != null) {
             String[] fields = line.split(",");
@@ -184,14 +68,16 @@ public class Main {
         System.out.println("Data Loaded.");
     }
 
+
+    // To save data to files
     public static void saveData(Department department) throws IOException {
         
         System.out.println("Saving Data...");
         
         // Initializing Buffers
-        BufferedWriter pcWriter = new BufferedWriter(new FileWriter(new File(pcFilePath)));
-        BufferedWriter employeeWriter = new BufferedWriter(new FileWriter(new File(employeeFilePath)));
-        BufferedWriter labWriter = new BufferedWriter(new FileWriter(new File(labFilePath)));
+        BufferedWriter pcWriter = new BufferedWriter(new FileWriter(pcFile));
+        BufferedWriter employeeWriter = new BufferedWriter(new FileWriter(employeeFile));
+        BufferedWriter labWriter = new BufferedWriter(new FileWriter(labFile));
 
         // Writing Headers
         pcWriter.write("ID,Name,LCD,RAM,SSD,GPU,LabID");
---
+++ Menu.java
@@ -0,0 +1,149 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void menu(Department department) {
+
+        menu: while (true) {
+            System.out.println(String.format("%32s", "").replace(" ", "="));
+            System.out.println(String.format("%17s", "MENU"));
+            System.out.println(String.format("%32s", "").replace(" ", "="));
+            System.out.println("1. Add a Lab");
+            System.out.println("2. Remove a Lab");
+            System.out.println("3. Add PC to a Lab");
+            System.out.println("4. Remove PC from a Lab");
+            System.out.println("5. Print Labs");
+            System.out.println("6. Print PCs of a Lab");
+            System.out.println("7. Print Department Info");
+            System.out.println("8. Exit Program");
+            System.out.println(String.format("%32s", "").replace(" ", "="));
+            System.out.println("Enter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();  // Ignore this
+            System.out.println(String.format("%32s", "").replace(" ", "="));
+
+            switch (choice) {
+                case 1:
+                    Menu.addLab(department);
+                    if (Menu.navigate() == 0) {break menu;}
+                    break;
+                case 2:
+                    Menu.removeLab(department);
+                    if (Menu.navigate() == 0) {break menu;}
+                    break;
+                case 3:
+                    Menu.addPC(department);
+                    if (Menu.navigate() == 0) {break menu;}
+                    break;
+                case 4:
+                    Menu.removePC(department);
+                    if (Menu.navigate() == 0) {break menu;}
+                    break;
+                case 5:
+                    department.printLabs();
+                    if (Menu.navigate() == 0) {break menu;}
+                    break;
+                case 6:
+                    Menu.printPCs(department);
+                    if (Menu.navigate() == 0) {break menu;}
+                    break;
+                case 7:
+                    System.out.println(department);
+                    if (Menu.navigate() == 0) {break menu;}
+                    break;
+                case 8:
+                    sc.close();
+                    break menu;
+                default:
+                    System.out.println("Invalid Input!");
+                    break;
+            }
+        }
+    }
+
+    // MENU Helper Functions
+
+    public static void addLab(Department department) {
+        System.out.println("Enter Lab Name: ");
+        String labName = sc.nextLine();
+        System.out.println("Enter Lab Attendant First Name: ");
+        String labAFirstName = sc.nextLine();
+        System.out.println("Enter Lab Attendant Last Name: ");
+        String labALastNAme = sc.nextLine();
+        department.addLab(new Lab(labName, new Employee(labAFirstName, labALastNAme, "Lab Attendant")));
+        System.out.println("Lab added successfully!\n");
+    }
+
+    public static void removeLab(Department department) {
+        System.out.println("Enter Lab Name: ");
+        String name = sc.nextLine();
+        int index = department.searchLab(name);
+        if (index != -1) {
+            department.removeLab(department.getLabs()[index]);
+            System.out.println("Lab removed successfully!\n");
+        } else {
+            System.out.println("Lab not found!\n");
+        }
+    }
+
+    public static void addPC(Department department) {
+        System.out.println("Enter Lab Name: ");
+        String labName1 = sc.nextLine();
+        int index1 = department.searchLab(labName1);
+        if (index1 != -1) {
+            System.out.println("Enter PC Name: ");
+            String pcName = sc.nextLine();
+            System.out.println("Enter LCD Name: ");
+            String monitorName = sc.nextLine();
+            System.out.println("Enter RAM Size (GB): ");
+            int ramSize = sc.nextInt();
+            System.out.println("Enter SSD Size (GB): ");
+            int hardDiskSize = sc.nextInt();
+            System.out.println("Is PC has a graphic card? (true/false): ");
+            boolean hasGraphicCard = sc.nextBoolean();
+            department.getLabs()[index1].addPC(new PC(pcName, monitorName, ramSize, hardDiskSize, hasGraphicCard));
+            System.out.println("PC added successfully!\n");
+        } else {
+            System.out.println("Lab not found!\n");
+        }
+    }
+
+    public static void removePC(Department department) {
+        System.out.println("Enter Lab Name: ");
+        String labName2 = sc.nextLine();
+        int index2 = department.searchLab(labName2);
+        if (index2 != -1) {
+            System.out.println("Enter PC Name: ");
+            String pcName = sc.nextLine();
+            int index3 = department.getLabs()[index2].searchPC(pcName);
+            if (index3 != -1) {
+                department.getLabs()[index2].removePC(department.getLabs()[index2].getPCs()[index3]);
+                System.out.println("PC removed successfully!\n");
+            } else {
+                System.out.println("PC not found!\n");
+            }
+        } else {
+            System.out.println("Lab not found!\n");
+        }
+    }
+
+    public static void printPCs(Department department) {
+        System.out.println("Enter Lab Name: ");
+        String labName3 = sc.nextLine();
+        int index4 = department.searchLab(labName3);
+        if (index4 != -1) {
+            department.getLabs()[index4].printPCs();
+        } else {
+            System.out.println("Lab not found!\n");
+        }
+    }
+
+    public static int navigate() {
+        System.out.println("Enter:\n\t(1) to go back to Menu\n\t(0) to exit program");
+        int option = sc.nextInt();
+        sc.nextLine();
+        return option;
+    }
+}
