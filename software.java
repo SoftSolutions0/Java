7213a8f11b034d9fe8b393933eaca77602cd3d8f
ahmed <ahmadabdullah4919@gmail.com>
2022-10-18 13:29:32 +0500

    Create Software Class

---
+++ software.java
@@ -0,0 +1,14 @@
+public class software {
+    
+    // Not completed yet
+    private String name, version, description, url, author, platform;
+
+    public software(String name, String version, String description, String url, String author, String platform) {
+        this.name = name;
+        this.version = version;
+        this.description = description;
+        this.url = url;
+        this.author = author;
+        this.platform = platform;
+    }
+}
