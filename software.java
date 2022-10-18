292489d9ef1a773de8b7dda280da5cdbde7a2e4f
ahmed <ahmadabdullah4919@gmail.com>
2022-10-18 13:32:02 +0500

    Update

--- software.java
+++ software.java
@@ -1,9 +1,9 @@
-public class software {
+public class Software {
     
     // Not completed yet
     private String name, version, description, url, author, platform;
 
-    public software(String name, String version, String description, String url, String author, String platform) {
+    public Software(String name, String version, String description, String url, String author, String platform) {
         this.name = name;
         this.version = version;
         this.description = description;
