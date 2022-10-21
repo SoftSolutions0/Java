6ee5c099d6a034c8a14697e4de6fe6b16f6a46d6
ahmed <ahmadabdullah4919@gmail.com>
2022-10-21 21:28:22 +0500

    Complete Software Class

--- Software.java
+++ Software.java
@@ -1,46 +1,66 @@
 public class Software {
     
-    // Not completed yet
-    private String name, version, description, url, author, platform;
+    private String name, version, type;
+    private int size;
 
-    public Software(String name, String version, String description, String url, String author, String platform) {
+    public Software(String name, String version, int size, String type) {
         this.name = name;
         this.version = version;
-        this.description = description;
-        this.url = url;
-        this.author = author;
-        this.platform = platform;
+        this.size = size;
+        this.type = type;
+    }
+
+    public Software(Software other) {
+        this.name = other.getName();
+        this.version = other.getVersion();
+        this.size = other.getSize();
+        this.type = other.getType();
     }
 
     public String getName() {
         return name;
     }
 
+    public int getSize() {
+        return size;
+    }
+
     public String getVersion() {
         return version;
     }
 
-    public String getDescription() {
-        return description;
+    public String getType() {
+        return type;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getPlatform() {
-        return platform;
+    public void setType(String type) {
+        this.type = type;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
+    public void setSize(int size) {
+        this.size = size;
+    }
+
     public void setVersion(String version) {
         this.version = version;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return new Software(this);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-22s %-10s %7s %6d", name, type, version, size);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return this.name.equals(((Software) obj).getName()) && this.version.equals(((Software) obj).getVersion());
     }
 }
